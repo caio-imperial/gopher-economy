937df1713f1782c4f4e4646615e514affa5f6dbf
caio.imperial <scaioimperial@gmail.com>
2025-01-05 16:49:31 -0300

    feat: init logger in main.go

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"log"
+
 	"github.com/caiosilvestre/gopher-economy/bot"
+	"github.com/caiosilvestre/gopher-economy/logger"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load(".env")
+
+	err := logger.NewLogger()
+	if err != nil {
+		log.Fatalf("Erro ao configurar o logger: %v", err)
+	}
+
+	appLogger := logger.GetLogger()
+
+	appLogger.Info("Application initialized successfully!")
+
+	defer appLogger.Sync()
+
 	bot.Init()
 }
